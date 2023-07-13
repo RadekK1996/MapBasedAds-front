a944969cda5e81f5580384e6c1f6a26e0dc416a2
Radosław Kowalski <ladekesl@gmail.com>
2023-07-13 14:25:30 +0200

    Added Link and  hover effect to "Mega ogłoszenia" text.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,10 @@
 import React, {FormEvent, useContext, useState} from 'react';
 import {Btn} from "../common/Btn";
 import {SearchContext} from "../../contexts/search.context";
+import {Link} from "react-router-dom";
 
 import './Header.css';
+
 
 
 export const Header = () => {
@@ -16,9 +18,11 @@ export const Header = () => {
 
     return (
         <header>
-            <h1>
-                <strong>Mega </strong> Ogloszenia
-            </h1>
+            <Link to="/">
+                <h1>
+                    <strong>Mega </strong> Ogloszenia
+                </h1>
+            </Link>
             <Btn to='/add' text='Dodaj ogloszenie'/>
             <form className="search" onSubmit={setSearchFromLocalState}>
                 <input type="text" value={inputVal} onChange={e => setInputVal(e.target.value)}/> <Btn text='Szukaj'/>
